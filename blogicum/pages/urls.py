48370630833af38f33e109e6206303c5f1dc23ba
Alexander Kunin <K.u.n.i.n@yandex.ru>
2024-05-16 15:29:09 +0300

    Еще одна мелкая правка перед 2 ревью

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 app_name = 'pages'
-handler404 = 'pages.views.handler404'
+handler404 = 'pages.views.page_not_found'
 
 urlpatterns = [
     path('about/', views.AboutView.as_view(), name='about'),
