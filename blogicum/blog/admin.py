138750be78aba583dcc7246a5d2d8a1292823701
Alexander Kunin <K.u.n.i.n@yandex.ru>
2024-05-16 13:28:45 +0300

    Все замечания устранены кроме вьюхи.

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,28 +1,40 @@
 from django.contrib import admin
 from .models import Category, Location, Post
 
-
+# Установка отображения пустых значений в админке.
 admin.site.empty_value_display = 'Не задано'
 
 
 class PostInline(admin.TabularInline):
+    """
+    Определение Inline-класса, который используется
+    для создания встроенных форм для связанных объектов Post.
+    """
+
     model = Post
+    # Количество дополнительных форм для ввода.
     extra = 0
 
 
 class CategoryAdmin(admin.ModelAdmin):
+    """Класс администрирования для модели Category."""
+
     inlines = (
         PostInline,
     )
 
 
 class LocationAdmin(admin.ModelAdmin):
+    """Класс администрирования для модели Location."""
+
     inlines = (
         PostInline,
     )
 
 
 class PostAdmin(admin.ModelAdmin):
+    """Класс администрирования для модели Post."""
+
     list_display = (
         'title',
         'text',
@@ -44,6 +56,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display_links = ('title',)
 
 
+# Регистрация моделей в админке.
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Location, LocationAdmin)
